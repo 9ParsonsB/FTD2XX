a0cc222fd0ccde96d0a61cd49c4260e48a4b9ae6
Ben Parsons <9parsonsb@gmail.com>
2022-05-22 12:55:32 +1000

    Add Constants as enums

---
+++ FTD2XX/Constants/FTDI_Constants.cs
@@ -0,0 +1,16 @@
+namespace FTD2XX.Constants;
+
+public static class FTDI_Constants
+{
+    #region DEFAULT_VALUES
+
+    public const uint FT_DEFAULT_BAUD_RATE = 9600;
+    public const uint FT_DEFAULT_DEADMAN_TIMEOUT = 5000;
+    public const int FT_COM_PORT_NOT_ASSIGNED = -1;
+    public const uint FT_DEFAULT_IN_TRANSFER_SIZE = 0x1000;
+    public const uint FT_DEFAULT_OUT_TRANSFER_SIZE = 0x1000;
+    public const byte FT_DEFAULT_LATENCY = 16;
+    public const uint FT_DEFAULT_DEVICE_ID = 0x04036001;
+    
+    #endregion
+}
